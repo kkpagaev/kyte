e5efc03098747fe630a1e6b2a0fd0b6fc5a1f1cf
kkpagaev <kkpagaev@gmail.com>
2023-03-29 22:24:44 +0300

    user init

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "home#index"
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 end
