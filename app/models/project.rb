58933864f05c51b7dfef6e67187e1854c7b31f7b
kkpagaev <kkpagaev@gmail.com>
2023-04-18 20:54:02 +0300

    fix block end

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ApplicationRecord
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [500, 500]
+  end
+
   has_many :repositories
   has_many :users, through: :repositories
 end
