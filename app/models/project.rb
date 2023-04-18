d6dd0653dbf69bdd74937e305941e1cd0f1d3497
Yehor Bilokin <belokonegor2000@gmail.com>
2023-04-18 17:15:54 +0000

    Update project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
-  has_one_attached :avatar
-
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [500, 500]
   has_many :repositories
   has_many :users, through: :repositories
 end
